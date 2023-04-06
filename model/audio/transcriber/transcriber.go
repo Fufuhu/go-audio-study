b08592230bf9c32c9007145b87735ed098d6528c
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2023-04-06 23:46:24 +0900

    Transcribeクライアントの追加

--- model/audio/transcriber/transcriber.go
+++ model/audio/transcriber/transcriber.go
@@ -1,13 +1,35 @@
 package transcriber
 
+import (
+	"context"
+	"github.com/Fufuhu/go-audio-study/util/logging"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/transcribe"
+	"go.uber.org/zap"
+)
+
 type AudioTranscriberConfig struct{}
 
 type AudioTranscriber struct {
 	config *AudioTranscriberConfig
+	client *transcribe.Client
 }
 
-func GetAudioTranscriber(config *AudioTranscriberConfig) *AudioTranscriber {
-	return &AudioTranscriber{config: config}
+func GetAudioTranscriber(c *AudioTranscriberConfig) (*AudioTranscriber, error) {
+	logger := logging.GetLogger()
+	defer func(logger *zap.Logger) {
+		_ = logger.Sync()
+	}(logger)
+
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		logger.Warn(err.Error())
+		return nil, err
+	}
+
+	return &AudioTranscriber{
+		config: c,
+		client: transcribe.NewFromConfig(cfg)}, nil
 }
 
 func GetDefaultAudioTranscriberConfig() *AudioTranscriberConfig {
