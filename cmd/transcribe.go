776ca34cd41360e2999b7c536151e7ad501cd591
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2023-04-06 23:24:00 +0900

    add transcribe subcommand

---
+++ cmd/transcribe.go
@@ -0,0 +1,41 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// transcribeCmd represents the transcribe command
+var transcribeCmd = &cobra.Command{
+	Use:   "transcribe",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("transcribe called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(transcribeCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// transcribeCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// transcribeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	transcribeCmd.Flags().StringVarP(&filePath, "filePath", "f", "", "file path to transcribe")
+	transcribeCmd.MarkFlagRequired("filePath")
+}
