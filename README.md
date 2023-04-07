8bf42f0aa86c02696636daaf3337f1ba7560dc47
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2023-04-08 00:19:13 +0900

    speakサブコマンドの実装

--- README.md
+++ README.md
@@ -15,3 +15,10 @@ macOS Monterey 12.6.3の場合は
 ```bash
 brew install portdudio
 ```
+
+https://github.com/hegedustibor/htgo-tts の動作に、
+https://github.com/hajimehoshi/oto/ を使っているので以下を実行
+
+```bash
+sudo apt install libasound2-dev
+```
---
+++ cmd/speak.go
@@ -0,0 +1,45 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	htgotts "github.com/hegedustibor/htgo-tts"
+	"github.com/hegedustibor/htgo-tts/handlers"
+	"github.com/hegedustibor/htgo-tts/voices"
+	"github.com/spf13/cobra"
+)
+
+// speakCmd represents the speak command
+var speakCmd = &cobra.Command{
+	Use:   "speak",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		speech := htgotts.Speech{Folder: "audio", Language: voices.Japanese, Handler: &handlers.Native{}}
+		speech.Speak(sentence)
+	},
+}
+
+var sentence string
+
+func init() {
+	rootCmd.AddCommand(speakCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// speakCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// speakCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	speakCmd.Flags().StringVarP(&sentence, "sentence", "s", "", "sentence to speak")
+	speakCmd.MarkFlagRequired("sentence")
+}
